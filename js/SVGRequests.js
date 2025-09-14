1a9aaf0f10144c7213a9b41a1474983c671e3311
jan Talisoso <38190993+Ists-Cilveks@users.noreply.github.com>
2025-09-14 16:10:48 +0300

    Make SVG requests asynchronous
    What exactly does this affect? I dunno. But it still works, and it shuts up the warning in the console.

--- js/SVGRequests.js
+++ js/SVGRequests.js
@@ -5,8 +5,8 @@ document.body.appendChild(invisibleSVGContainer)
 
 function insertSVG(fileName) {
   // using https://stackoverflow.com/a/14070928/10630826
-  xhr = new XMLHttpRequest()
-  xhr.open("GET", fileName, false)
+  const xhr = new XMLHttpRequest()
+  xhr.open("GET", fileName, true)
   xhr.overrideMimeType("image/svg+xml") // just to be on the safe side; not needed if your server delivers SVG with correct MIME type
   xhr.onload = function(e) {
     // You might also want to check for xhr.readyState/xhr.status here
