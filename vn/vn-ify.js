615917b052431a147ed86dd15e6b29c03fbe24b5
jan Talisoso <38190993+Ists-Cilveks@users.noreply.github.com>
2025-09-19 13:13:20 +0300

    vn-ify is now js instead of html
    Can more easily create new VN type pages by importing vn-ify.js

---
+++ vn/vn-ify.js
@@ -0,0 +1,142 @@
+// Make necessary elements if not defined in the given document
+if (!document.getElementById('vn-stage')) {
+  let stage = document.createElement('div')
+  stage.id = 'vn-stage'
+  stage.addEventListener("click", progressText)
+  stage.lang = "en" // TODO: how to handle translations?
+  document.body.appendChild(stage)
+  
+  let td = document.createElement('div')
+  td.id = 'vn-temp-display'
+  stage.appendChild(td)
+
+  let tb = document.createElement('div')
+  tb.id = 'vn-textbox'
+  stage.appendChild(tb)
+
+  let clickToProgress = document.createElement('p')
+  clickToProgress.innerHTML = "<p>Click to progress the text, or <a onclick='viewInline()'>click here</a> to view this in a normal format.</p>"
+  tb.appendChild(clickToProgress)
+}
+// Get necessary elements (potentially freshly made ↑)
+const vnStage = document.getElementById('vn-stage')
+const textbox = document.getElementById('vn-textbox')
+const tempDisplay = document.getElementById('vn-temp-display')
+const allContent = document.getElementById('vn-content')
+let lastAddedContent
+let contentDiv = document.getElementById("vn-content").firstElementChild
+
+var characters = []
+class Character {
+  constructor(name, displayName, accessibleName, container, svgPath) {
+    this.name = name
+    this.displayName = displayName
+    this.accessibleName = accessibleName
+    // TODO: <span role="img" aria-label="Kit">K¡t</span>
+    if (container) {
+      this.container = container
+    } else {
+      this.container = document.createElement("div")
+      this.container.className = "character-container"
+      this.container.id = name+"-container"
+      document.getElementById("vn-stage").appendChild(this.container)
+    }
+    if (svgPath) {
+      insertSVG(svgPath, this.container, function(e, characterSVG) {
+        characterSVG.setAttribute("height", "80vh") // FIXME: images should be scaled equally (bigger images remain bigger), instead of all being a certain height
+        characterSVG.removeAttribute("width")
+      })
+    }
+    this.expression = "neutral"
+  }
+
+  setExpression (newExp) {
+    const currentExpressionID = this.name+"-"+this.expression
+    const newExpressionID = this.name+"-"+newExp
+    document.getElementById(currentExpressionID).style.display = "none"
+    document.getElementById(newExpressionID).style.display = "inline"
+    this.expression = newExp
+  }
+}
+
+characters.push(new Character("kit", "K¡t", "Kit", undefined, "kit/kit.svg"))
+
+function viewInline() {
+  vnStage.style.display = "none"
+  allContent.style.display = "block"
+  takeNextElement = function(){} // Hack to stop the viewInline link from removing the first line before it's seen
+}
+
+function takeNextElement() {
+  let newContent = contentDiv.firstElementChild
+  if (!newContent) {
+    let newContentDiv = contentDiv.nextElementSibling
+    if(!newContentDiv) { // out of content
+      return
+    }
+    contentDiv = newContentDiv
+    newContent = contentDiv.firstElementChild
+  }
+  const classes = newContent.className.split(" ")
+
+  // Add to appropriate place
+  if (classes[0]=="vn-show-off") { // FIXME: lazy ass check, i better remember to fix this before it gives me a wild goose chase
+    tempDisplay.appendChild(newContent)
+  } else {
+    textbox.appendChild(newContent)
+  }
+  lastAddedContent = newContent
+
+  // Look for expressions
+  for (const char of characters) {
+    let emotion
+    for (const cl of classes) {
+      if (cl.startsWith(char.name+"-")) {
+        emotion = cl.slice(char.name.length+1)
+        break
+      }
+    }
+    
+    if (emotion) {
+      char.setExpression(emotion)
+    }
+  }
+
+  for (const cl of classes) {
+    if (cl=="vn-take-next") {
+      takeNextElement()
+    }
+  }
+}
+function progressText() {
+  // Escape from hidden state
+  if (textbox.style.display == "none") {
+    textbox.style.display = "initial"
+    return
+  }
+
+  // TODO: if animating text, skip to end of animation
+
+  // TODO: how to prevent this from happening if there is no more text to show? Should I have a separate final textbox state instead of staying on the last line?
+  textbox.textContent = ""
+  tempDisplay.textContent = ""
+
+  // Get next element
+  takeNextElement()
+}
+
+document.addEventListener("keydown", function (event) {
+  let key = event.key
+  switch (key) {
+    case "ArrowRight":
+    case "Enter":
+    case " ":
+      progressText()
+      break
+  }
+  key = key.toLocaleLowerCase()
+  switch (key) {
+    case "h":
+      textbox.style.display = textbox.style.display == "none" ? "initial" : "none"
+  }
+})
